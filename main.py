28eb44ca2a0d9aebfe1f5f15dc93526958f88607
Adadazx <caioedavi2010@gmail.com>
2026-07-09 11:37:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from cadastrar_aluno import cadastrar_aluno
 from listar_aluno import listar_aluno
 from notas import cadastrar_notas
+from listar_notas import listar_notas
+from editar_aluno import editar_aluno
+from remover_aluno import remover_aluno
 
 while True:
     print("\n1 - Cadastrar aluno")
     print("2 - Listar alunos")
-    print("3 - cadastrar notas")
-    print("3 - Sair")
+    print("3 - Cadastrar notas")
+    print("4 - listar notas")
+    print("5 - editar aluno")
+    print("6 - remover aluno")
+    print("7 - Sair")
 
     opcao = input("Escolha: ")
 
@@ -19,36 +25,44 @@ while True:
     elif opcao == "3":
         cadastrar_notas()
 
-    elif opcao == "3":
+    elif opcao == "4":
+        listar_notas()
+
+    elif opcao =="5":
+        editar_aluno()
+
+    elif opcao == "6":
+        remover_aluno()
+
+    elif opcao == "7":
         break
+
 
     else:
         print("Opção inválida.")
-
-
-    print("\nDeseja voltar?")
-    print("1 - Sim")
-    print("2 - Não")
-
-    opcao = input("Escolha: ").strip()
-
-    if opcao == "":
-        print("Campo vazio.")
         continue
 
-    if not opcao.isdigit():
-        print("Digite apenas números.")
-        continue
+    while True:
+        print("\nDeseja voltar?")
+        print("1 - Sim")
+        print("2 - Não")
 
-    if opcao == "1":
-        
-        continue
+        voltar = input("Escolha: ").strip()
 
-    elif opcao == "2":
-        
-        break 
+        if voltar == "":
+            print("Campo vazio.")
+            continue
 
-    else:
-        print("Escolha apenas 1 ou 2.")
+        if not voltar.isdigit():
+            print("Digite apenas números.")
+            continue
 
+        if voltar == "1":
+            break  
+
+        elif voltar == "2":
+            exit()  
+
+        else:
+            print("Escolha apenas 1 ou 2.")
 
