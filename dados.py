94cb594a4bc13f787bf26c9939fa98be6cac3c54
Adadazx <caioedavi2010@gmail.com>
2026-07-09 16:25:54 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -1,20 +1,23 @@
 import json
 
-dados = {
-    "nome": "Ana",
-    "idade": 28,
-    "ativo": True,
-    "habilidades": ["python", "java"]
-}
-
-with open ('dados.json', 'w', encoding='utf-8') as arquivo:
-    json.dump(dados,arquivo, ensure_ascii=False, indent=4)
-
-def ler_dados():
-    with open('dados.json', 'r', encoding='utf-8') as arquivo:
-        dados_lidos = json.load(arquivo)
-
-    print(dados_lidos["idade"])
+arquivo = "dados.json"
 
 
-ler_dados()
+def salvar(dados):
+    with open(arquivo, "w", encoding="utf-8") as f:
+        json.dump(dados, f, indent=4, ensure_ascii=False)
+
+
+def carregar():
+    try:
+        with open(arquivo, "r", encoding="utf-8") as f:
+            return json.load(f)
+
+    except FileNotFoundError:
+        return {
+            "alunos": [],
+            "notas": [],
+            "medias": [],
+            "situacoes": []
+        }
+    
